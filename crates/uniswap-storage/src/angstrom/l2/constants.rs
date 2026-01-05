b13b209a08defe4791b3ee3b4ecc5e57388ac986
jnoorchashm37 <joseph@sorellalabs.xyz>
2026-01-05 17:03:48 -0500

    updated addresses

--- crates/uniswap-storage/src/angstrom/l2/constants.rs
+++ crates/uniswap-storage/src/angstrom/l2/constants.rs
@@ -54,9 +54,9 @@ impl AngstromL2Constants {
 }
 
 pub const ANGSTROM_L2_CONSTANTS_BASE_MAINNET: AngstromL2Constants = AngstromL2Constants {
-    angstrom_l2_factory:    address!("0x000000000000a0220e791bAe32b16C0406980C42"),
-    hook_address_miner:     address!("0x801988754b99f142C5C5CA19ca9656CFf31A898a"),
-    angstrom_deploy_block:  36617377,
+    angstrom_l2_factory:    address!("0x0090B13F34D024597c478e92b5ce683649bEF9cA"),
+    hook_address_miner:     address!("0xfdb9d8b8cc46346a58bc4fcc785f799d71dd977d"),
+    angstrom_deploy_block:  40425432,
     chain_id:               8453,
     angstrom_eip712_domain: alloy_sol_types::eip712_domain!(
         name: "Angstrom",
@@ -68,7 +68,7 @@ pub const ANGSTROM_L2_CONSTANTS_BASE_MAINNET: AngstromL2Constants = AngstromL2Co
 };
 
 pub const ANGSTROM_L2_CONSTANTS_UNICHAIN_MAINNET: AngstromL2Constants = AngstromL2Constants {
-    angstrom_l2_factory:    address!("0x000000000000a0220e791bae32b16c0406980c42"),
+    angstrom_l2_factory:    address!("0x000000000000a0220e791bAe32b16C0406980C42"),
     hook_address_miner:     address!("0x98bD6680eE62C006730A5a4dFB2f0Da6645A10f5"),
     angstrom_deploy_block:  29275745,
     chain_id:               130,
